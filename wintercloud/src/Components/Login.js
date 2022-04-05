0d6e728f2e8294a4bd8828084ee2e76a2c701895
unknown <sn493898@dal.ca>
2022-04-05 14:54:23 -0300

    Register & email verification with aws auth function

--- wintercloud/src/Components/Login.js
+++
@@ -1,9 +0,0 @@
-import './Login.css';
-function Login() {
- 
-  return (
-    <p>works!</p>
-  );
-}
-
-export default Login;
--- wintercloud/src/Components/Register.js
+++ wintercloud/src/Components/Register.js
@@ -1,112 +1,124 @@
- import React from "react";
- import { useState } from "react";
- import { NavLink } from "react-router-dom";
- import { useNavigate } from "react-router-dom";
- import Button from "react-bootstrap/Button";
- import Form from "react-bootstrap/Form";
- import NavBar from "./NavBar";
- 
- function Register() {
-   const navigate = useNavigate();
- 
-   const nameRegex = /^[A-Za-z]+$/;
-   const emailRegex = /^[0-9a-zA-Z._-]+@[0-9a-zA-Z._-]+\.[(0-9a-zA-Z._)]+$/;
-   const passwordRegex = /^.{8,}$/;
- 
-   const [errorMessage, setErrorMessage] = useState("");
- 
-   const [data, setData] = useState({
-     firstName: "",
-     lastName: "",
-     email: "",
-     password: "",
-   });
- 
-   const handleChange = ({ target: { name, value } }) => {
-     setData((prev) => ({ ...prev, [name]: value }));
-   };
- 
-   const handleSubmit = async (e) => {
-     
-   };
- 
-   return (
-       <div>
-           <NavBar />
-       
-     <Form onSubmit={handleSubmit}>
-       <div className="outer">
-         <div className="inner">
-           <h3>Register</h3>
-           <br />
-           <div className="form-group">
-             <input
-               type="text"
-               className="form-control inside-textbox form-field-color"
-               placeholder="First name"
-               name="firstName"
-               value={data.firstName}
-               onChange={handleChange}
-             />
-           </div>
-           <br />
-           <div className="form-group">
-             <input
-               type="text"
-               className="form-control inside-textbox form-field-color"
-               placeholder="Last name"
-               name="lastName"
-               value={data.lastName}
-               onChange={handleChange}
-             />
-           </div>
-           <br />
-           <div className="form-group">
-             <input
-               type="email"
-               className="form-control inside-textbox form-field-color"
-               placeholder="Enter email"
-               name="email"
-               value={data.email}
-               onChange={handleChange}
-             />
-           </div>
-           <br />
-           <div className="form-group">
-             <input
-               type="password"
-               className="form-control inside-textbox form-field-color"
-               placeholder="Enter password"
-               name="password"
-               value={data.password}
-               onChange={handleChange}
-             />
-           </div>
-           <br />
-           {errorMessage.length > 0 ? (
-             <p className="button-center" style={{ color: "red" }}>
-               {errorMessage}
-             </p>
-           ) : null}
-           <div className="button-center">
-             <Button variant="primary" type="submit">
-               Register
-             </Button>
-           </div>
-           <div>
-             <hr className="simple-line" />
-           </div>
-           <div className="button-center forgot-password text-right">
-             <p className="forgot-password text-right">
-               Already registered ? <NavLink to="/login">log in</NavLink>
-             </p>
-           </div>
-         </div>
-       </div>
-     </Form>
-     </div>
-   );
- }
- 
- export default Register;
- 
+import React, { Component } from 'react';
+import { Auth } from "aws-amplify";
+
+class Register extends Component {
+  state = {
+    reg_username: "",
+    reg_email: "",
+    password: "",
+    confirmpassword: "",
+    isErrorFree: true
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    const formvalid = this.validateFormFields()
+    if(formvalid){
+      const { reg_username, password, reg_email } = this.state;
+      let job = {
+        username : reg_username,
+        password : password,
+        attributes : {
+          email: reg_email
+        }
+      }
+      try {
+        const awsResponseObject = await Auth.signUp(job);
+        this.props.history.push("/emailVerification");
+      } catch (form_error) {
+        this.validateFormFields(form_error)
+      }
+    }
+
+  }
+
+  validateFormFields = (cognitoErrors) => {
+    this.setState({isErrorFree : true});
+    if(cognitoErrors){
+      alert(cognitoErrors)
+      this.setState({
+        isErrorFree: false
+      })
+      return false;
+    }else{
+        if( this.state.reg_username.length == 0 || this.state.reg_email.length == 0 || this.state.password.length == 0 || this.state.confirmpassword.length == 0)
+        {
+          alert("Please enter all fields");
+          this.setState({
+            isErrorFree: false
+          })
+          return false
+        }else if (this.state.reg_email.match("/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/")){
+          alert("Invalid email pattern")
+          this.setState({
+            isErrorFree: false
+          })
+          return false
+        }else if(this.state.password.match("/^(?=.[A-Za-z])(?=.\d)(?=.[@$!%#?&])[A-Za-z\d@$!%*#?&]{8,}$/")){
+          alert("Password should be minimum 8 letters with a special character and a upper case")
+          this.setState({
+            isErrorFree: false
+          })
+          return false
+        }else if(this.state.password != this.state.confirmpassword){
+          alert("Password mismatched")
+          this.setState({
+            isErrorFree: false
+          })
+          return false
+        }
+    }
+    return true
+  }
+
+
+  onInputChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+    document.getElementById(event.target.id).classList.remove("is-danger");
+  }
+
+  render() {
+    return (
+      <section className="section formfield">
+        <div className="container">
+          <h6 className='welcome_heading'>Welcome to Winter Cloud</h6>
+          <h1>Register</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div className="field">
+              <p className="control">
+                <input className="input" id="username" name="reg_username" placeholder="Enter username" type="text" value={this.state.reg_username} onChange={this.onInputChange}/>
+              </p>
+            </div>
+            <div className="field">
+              <p className="control">
+                <input className="input" id="email" name="reg_email" placeholder="Enter email" type="text" value={this.state.reg_email} onChange={this.onInputChange}/>
+              </p>
+            </div>
+            <div className="field">
+              <p className="control">
+                <input className="input" id="password" name="password" placeholder="Password"  type="password" value={this.state.password} onChange={this.onInputChange}/>
+              </p>
+            </div>
+            <div className="field">
+              <p className="control">
+                <input className="input" id="confirmpassword" name="confirmpassword" placeholder="Confirm password"  type="password" value={this.state.confirmpassword} onChange={this.onInputChange}/>
+              </p>
+            </div>
+            <p>Have an account already? <a href='/login'>Login</a></p>
+            <div className="field">
+              <p className="control">
+                <button className="button is-link">
+                  <strong>Submit</strong>
+                </button>
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Register;
